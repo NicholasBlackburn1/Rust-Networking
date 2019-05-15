2a1aca7d8abdbecce1d6fa8e9b2ddc2934b20db1
nicholasblackburn1 <nickblackburn02@gmail.com>
2019-05-15 13:29:44 -0400

    added tcp server

--- testtcp/src/main.rs
+++ testtcp/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::net::TcpStream;
+
+fn main(){
+// Server to connect to
+    let mut stream = TcpStream::connect("127.0.0.1:100100")?;
+
+    stream.write(&[1])?;
+    stream.read(&mut [0; 128])?;
+    Ok(())
+} // the stream is closed here
